e0446fbb239e3f7b52cf1e47cc0ad8a103cb560f
Will Sewell <was111607@bitbucket.org>
2022-06-08 14:53:18 +0100

    Added everything else

--- src/main/java/com/example/hellospring/ExampleController.java
+++ src/main/java/com/example/hellospring/ExampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExampleController {
 
-  @GetMapping(value = "/hello")
+  @GetMapping(value = "/")
   public String getProduct() {
     return "Hello, World!";
   }
