c5a45502ff878545a95710419e8e16ba5bd08dfe
Will Sewell <was111607@bitbucket.org>
2022-06-08 12:42:05 +0100

    tidied format

--- src/main/java/com/example/hellospring/ExampleController.java
+++ src/main/java/com/example/hellospring/ExampleController.java
@@ -1,14 +1,14 @@
 package com.example.hellospring;
- 
- import org.springframework.web.bind.annotation.GetMapping;
- import org.springframework.web.bind.annotation.RestController;
- 
- @RestController
- public class ExampleController {
- 
-   @GetMapping(value = "/")
-   public String getProduct()
-   {
-     return "Hello, World!";
-   }
- }
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+  @GetMapping(value = "/")
+  public String getProduct() {
+    return "Hello, World!";
+  }
+
+}
