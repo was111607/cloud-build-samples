f941c2f2ac33613e30581757a5de0c2b372e1ded
Nathan Hall <37386274+hallnath1@users.noreply.github.com>
2022-06-08 12:21:13 +0100

    Update ExampleController.java

--- src/main/java/com/example/hellospring/ExampleController.java
+++ src/main/java/com/example/hellospring/ExampleController.java
@@ -1,6 +1,5 @@
 package com.example.hellospring;
 
-import org.springframework.beans.factory.annotation.Autowired;  
 import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.RestController;
 
