b59e064140005f0e5dc280ab089e5b6ccf4254b3
Will Sewell <was111607@bitbucket.org>
2022-06-08 13:13:46 +0100

    COmmented out run

--- src/main/java/com/example/hellospring/HelloSpringApplication.java
+++ src/main/java/com/example/hellospring/HelloSpringApplication.java
@@ -16,16 +16,17 @@
 
 package com.example.hellospring;
 
-import java.util.HashMap;
+// import java.util.HashMap;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class HelloSpringApplication implements CommandLineRunner {
+// public class HelloSpringApplication implements CommandLineRunner {
+public class HelloSpringApplication {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
